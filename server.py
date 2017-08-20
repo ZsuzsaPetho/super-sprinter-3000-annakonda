db9c5a9a91c62187b9052dd9f2214d01012b3e0f
Zsuzsa <zsuzsapethocodecool@gmail.com>
2017-08-20 17:20:58 +0200

    version 2

--- server.py
+++ server.py
@@ -54,9 +54,34 @@ def line_in_table(table):
     return sum(1 for line in table)
 
 
-def write_row_to_file(file_name, row):
+def write_row_to_file(file_name, list_):
+    row = ';'.join(list_)
     with open(file_name, "a") as file:
         file.write(row + "\n")
+
+
+def update_row_in_table(table, story_id, story_data):
+    for i in range(len(table)):
+                if table[i][0] == str(story_id):
+                    table[i] = story_data
+    return table
+
+
+def joining_data_from_submit(story_id, label_list, stat):
+    return [str(story_id)] + [request.form[item].replace("\r\n", " ") for item in label_list] + [status_converter
+                                                                                                 (request.form[stat])]
+
+
+def renumbering(table):
+    for i in range(len(table)):
+        table[i][0] = str(i+1)
+    return table
+
+
+def status_reconverted_table(table):
+    for i in range(len(table)):
+        table[i][-1] = status_re_converter(table[i][-1])
+    return table
 
 app = Flask(__name__)
 
@@ -71,51 +96,43 @@ def route_index(story_id=None):
         value_list = get_table_from_file(file_name)
         if request.method == 'GET':
             status_list = get_selected_from_table(value_list, story_id-1)
-            return render_template('form.html', title=title, action=action, button="Update", story_title=value_list[story_id-1][1],
-                                   user_story=value_list[story_id-1][2], acc_crit=value_list[story_id-1][3], 
+            return render_template('form.html', title=title, action=action, button="Update",
+                                   story_title=value_list[story_id-1][1],
+                                   user_story=value_list[story_id-1][2],
+                                   acc_crit=value_list[story_id-1][3],
                                    b_value=int(value_list[story_id-1][4]),
-                                   estimation=float(value_list[story_id-1][5]), status=status_list)
+                                   estimation=float(value_list[story_id-1][5]),
+                                   status=status_list)
         elif request.method == 'POST':
-            story_data = ([str(story_id)]+[request.form['story_title']]+[request.form['user_story'].replace("\r\n", " ")]+[request.form['acc_crit'].replace("\r\n", " ")]+
-                          [request.form['b_value']]+[request.form['estimation']]+[status_converter(request.form['status'])])
-            table = get_table_from_file(file_name)
-            for i in range(len(table)):
-                if table[i][0] == str(story_id):
-                    table[i] = story_data
+            story_data = joining_data_from_submit(1 + line_in_file(file_name),
+                                                  ['story_title', 'user_story', 'acc_crit', 'b_value', 'estimation'],
+                                                  'status')
+            table = update_row_in_table(get_table_from_file(file_name), story_id, story_data)
             write_table_to_file(file_name, table)
             return redirect(url_for('route_index'))
     else:
         title = "Add New Story"
         action = "/story"
         if request.method == 'POST':
-            id_ = str(1 + line_in_file(file_name))
-            story_data = ([id_]+[request.form['story_title']]+[request.form['user_story'].replace("\r\n", " ")]+
-                          [request.form['acc_crit'].replace("\r\n", " ")]+
-                          [request.form['b_value']]+[request.form['estimation']]+[status_converter(request.form['status'])])
-            row = ';'.join(story_data)
-            print(row)
-            write_row_to_file(file_name, row)
-            return render_template('form.html', title=title, status=["", "", "", "", ""], button="Create")
-        return render_template('form.html', title=title, status=["", "", "", "", ""], button="Create")
+            story_data = joining_data_from_submit(1 + line_in_file(file_name),
+                                                  ['story_title', 'user_story', 'acc_crit', 'b_value', 'estimation'],
+                                                  'status')
+            write_row_to_file(file_name, story_data)
+    return render_template('form.html', title=title, status=["", "", "", "", ""], button="Create")
 
 
 @app.route('/')
 @app.route('/list')
 def route_list():
-    value_list = get_table_from_file("data.csv")
-    for i in range(len(value_list)):
-        value_list[i][-1] = status_re_converter(value_list[i][-1])
+    value_list = status_reconverted_table(get_table_from_file("data.csv"))
     return render_template('list.html', table=value_list)
 
 
 @app.route('/list/<int:story_id>')
 def del_list(story_id=None):
     file_name = "data.csv"
-    value_list = get_table_from_file(file_name)
-    value_list = [line for line in value_list if line[0] != str(story_id)]
-    for i in range(len(value_list)):
-        value_list[i][0] = str(i+1)
-    write_table_to_file(file_name, value_list)
+    value_list = [line for line in get_table_from_file(file_name) if line[0] != str(story_id)]
+    write_table_to_file(file_name, renumbering(value_list))
     return redirect(url_for('route_list'))
 
 
